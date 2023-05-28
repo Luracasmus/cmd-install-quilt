002279e086547f65dc13c9008e7af1d663137691
Lura <77991691+Luracasmus@users.noreply.github.com>
2023-05-28 14:27:50 +0200

    Add files via upload

---
+++ install-quilt.cmd
@@ -0,0 +1,3 @@
+@echo off
+
+start %LocalAppData%\Packages\Microsoft.4297127D64EC6_8wekyb3d8bbwe\LocalCache\Local\runtime\java-runtime-gamma\windows-x64\java-runtime-gamma\bin\javaw.exe -jar quilt-installer.jar
